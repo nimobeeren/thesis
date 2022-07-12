cda1e4bc9d4210436384b969cd9627be3cb456a8
Nimo Beeren <nimobeeren@gmail.com>
2022-07-12 18:56:43 +0200

    enforce schema for movie nodes

--- janusgraph/src/main/java/com/github/nimobeeren/thesis/janusgraph/App.java
+++ janusgraph/src/main/java/com/github/nimobeeren/thesis/janusgraph/App.java
@@ -4,14 +4,20 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import org.janusgraph.core.JanusGraph;
 import org.janusgraph.core.JanusGraphFactory;
+import org.janusgraph.diskstorage.BackendException;
 
 public class App {
-  public static void main(String[] args) throws FileNotFoundException, IOException {
+  public static void main(String[] args) throws FileNotFoundException, IOException, BackendException {
     JanusGraphFactory.Builder config = JanusGraphFactory.build();
     config.set("storage.backend", "berkeleyje");
     config.set("storage.directory", "/var/lib/janusgraph/data");
-    
+    config.set("schema.default", "none");
     JanusGraph graph = config.open();
+
+    JanusGraphFactory.drop(graph);
+    System.out.println("Dropped graph");
+
+    graph = config.open();
     System.out.println("Opened graph");
 
     RecommendationsGraph recommendations = new RecommendationsGraph(args[0]);
--- janusgraph/src/main/java/com/github/nimobeeren/thesis/janusgraph/RecommendationsGraph.java
+++ janusgraph/src/main/java/com/github/nimobeeren/thesis/janusgraph/RecommendationsGraph.java
@@ -8,6 +8,7 @@ import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.janusgraph.core.JanusGraph;
 import org.janusgraph.core.JanusGraphVertex;
+import org.janusgraph.core.schema.JanusGraphManagement;
 
 public class RecommendationsGraph {
 
@@ -25,6 +26,26 @@ public class RecommendationsGraph {
   }
 
   public void load(JanusGraph graph) throws FileNotFoundException, IOException {
+    JanusGraphManagement mgmt = graph.openManagement();
+    mgmt.makeVertexLabel("Movie").make();
+    mgmt.makePropertyKey("budget").dataType(Long.class).make();
+    mgmt.makePropertyKey("countries").dataType(String.class).make(); // TODO: parse list
+    mgmt.makePropertyKey("imdbId").dataType(String.class).make();
+    mgmt.makePropertyKey("imdbRating").dataType(Float.class).make();
+    mgmt.makePropertyKey("imdbVotes").dataType(Long.class).make();
+    mgmt.makePropertyKey("languages").dataType(String.class).make(); // TODO: parse list
+    mgmt.makePropertyKey("movieId").dataType(String.class).make();
+    mgmt.makePropertyKey("plot").dataType(String.class).make();
+    mgmt.makePropertyKey("poster").dataType(String.class).make();
+    mgmt.makePropertyKey("released").dataType(String.class).make();
+    mgmt.makePropertyKey("revenue").dataType(Long.class).make();
+    mgmt.makePropertyKey("runtime").dataType(Short.class).make();
+    mgmt.makePropertyKey("title").dataType(String.class).make();
+    mgmt.makePropertyKey("tmdbId").dataType(String.class).make();
+    mgmt.makePropertyKey("url").dataType(String.class).make();
+    mgmt.makePropertyKey("year").dataType(Short.class).make();
+    mgmt.commit();
+
     Iterable<CSVRecord> movies = parseFile("movies.csv");
 
     for (CSVRecord movieRecord : movies) {
