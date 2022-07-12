beb319cb38ec4907431765dd2bb0caca5bccaad2
Nimo Beeren <nimobeeren@gmail.com>
2022-07-12 14:23:44 +0200

    add a vertex

--- janusgraph/src/main/java/com/github/nimobeeren/thesis/janusgraph/App.java
+++ janusgraph/src/main/java/com/github/nimobeeren/thesis/janusgraph/App.java
@@ -3,17 +3,16 @@ package com.github.nimobeeren.thesis.janusgraph;
 import org.janusgraph.core.JanusGraph;
 import org.janusgraph.core.JanusGraphFactory;
 
-// FIXME: The JanusGraph classes cannot be found when running in the container, either
-// have to add them to classpath manually (which won't work for the CSV library)
-// or package them in a jar
-
 public class App {
     public static void main(String[] args) {
         JanusGraphFactory.Builder config = JanusGraphFactory.build();
         config.set("storage.backend", "berkeleyje");
-        config.set("storage.directory", "var/lib/janusgraph/data");
+        config.set("storage.directory", "/var/lib/janusgraph/data");
         JanusGraph graph = config.open();
 
-        System.out.println(graph.traversal().V());
+        graph.addVertex("Person").property("hello", "world");
+        graph.tx().commit();
+
+        System.exit(0);
     }
 }
