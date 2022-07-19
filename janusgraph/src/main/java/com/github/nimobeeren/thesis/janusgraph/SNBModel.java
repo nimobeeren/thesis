b903887b67379167f91985308f3940166df9b507
Nimo Beeren <nimobeeren@gmail.com>
2022-07-19 11:28:54 +0200

    define snb schema in janusgraph

--- janusgraph/src/main/java/com/github/nimobeeren/thesis/janusgraph/SNBModel.java
+++ janusgraph/src/main/java/com/github/nimobeeren/thesis/janusgraph/SNBModel.java
@@ -1,33 +1,19 @@
 package com.github.nimobeeren.thesis.janusgraph;
 
-import static org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__.hasLabel;
-import static org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__.hasNot;
-import static org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__.out;
 import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.Date;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Set;
-import org.apache.commons.csv.CSVRecord;
 import org.apache.commons.lang.NotImplementedException;
-import org.apache.tinkerpop.gremlin.process.traversal.P;
-import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
-import org.apache.tinkerpop.gremlin.structure.Edge;
 import org.apache.tinkerpop.gremlin.structure.Element;
-import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.janusgraph.core.Cardinality;
 import org.janusgraph.core.EdgeLabel;
 import org.janusgraph.core.JanusGraph;
-import org.janusgraph.core.JanusGraphTransaction;
-import org.janusgraph.core.JanusGraphVertex;
 import org.janusgraph.core.Multiplicity;
 import org.janusgraph.core.PropertyKey;
 import org.janusgraph.core.VertexLabel;
 import org.janusgraph.core.schema.JanusGraphManagement;
-import org.janusgraph.graphdb.database.StandardJanusGraph;
-import org.janusgraph.graphdb.idmanagement.IDManager;
 
 public class SNBModel extends DataModel {
   SNBModel(JanusGraph graph) {
@@ -51,40 +37,141 @@ public class SNBModel extends DataModel {
     VertexLabel TagClass = mgmt.makeVertexLabel("TagClass").make();
 
     // Property keys with datatypes and cardinalities
-    PropertyKey creation_dateKey = mgmt.makePropertyKey("creation_date").dataType(Date.class).make();
-    PropertyKey location_ipKey = mgmt.makePropertyKey("location_ip").dataType(String.class).make();
-    PropertyKey browser_usedKey = mgmt.makePropertyKey("browser_used").dataType(String.class).make();
+    PropertyKey creationDateKey = mgmt.makePropertyKey("creationDate").dataType(Date.class).make();
+    PropertyKey locationIpKey = mgmt.makePropertyKey("locationIp").dataType(String.class).make();
+    PropertyKey browserUsedKey = mgmt.makePropertyKey("browserUsed").dataType(String.class).make();
     PropertyKey contentKey = mgmt.makePropertyKey("content").dataType(String.class).make();
     PropertyKey lengthKey = mgmt.makePropertyKey("length").dataType(Integer.class).make();
-    PropertyKey image_fileKey = mgmt.makePropertyKey("image_file").dataType(String.class).make();
-    PropertyKey langKey = mgmt.makePropertyKey("lang").dataType(String.class).make();
+    PropertyKey imageFileKey = mgmt.makePropertyKey("imageFile").dataType(String.class).make();
+    PropertyKey languageKey = mgmt.makePropertyKey("language").dataType(String.class).make();
     PropertyKey nameKey = mgmt.makePropertyKey("name").dataType(String.class).make();
     PropertyKey urlKey = mgmt.makePropertyKey("url").dataType(String.class).make();
     PropertyKey titleKey = mgmt.makePropertyKey("title").dataType(String.class).make();
-    PropertyKey first_nameKey = mgmt.makePropertyKey("first_name").dataType(String.class).make();
-    PropertyKey last_nameKey = mgmt.makePropertyKey("last_name").dataType(String.class).make();
+    PropertyKey firstNameKey = mgmt.makePropertyKey("firstName").dataType(String.class).make();
+    PropertyKey lastNameKey = mgmt.makePropertyKey("lastName").dataType(String.class).make();
     PropertyKey genderKey = mgmt.makePropertyKey("gender").dataType(String.class).make();
     PropertyKey birthdayKey = mgmt.makePropertyKey("birthday").dataType(Date.class).make();
-    PropertyKey speaksKey = mgmt.makePropertyKey("speaks").dataType(String.class).cardinality(Cardinality.SET).make();
-    PropertyKey emailKey = mgmt.makePropertyKey("email").dataType(String.class).cardinality(Cardinality.SET).make();
+    // Not modeled: set must have size >= 1
+    PropertyKey speaksKey =
+        mgmt.makePropertyKey("speaks").dataType(String.class).cardinality(Cardinality.SET).make();
+    // Not modeled: set must have size >= 1
+    PropertyKey emailKey =
+        mgmt.makePropertyKey("email").dataType(String.class).cardinality(Cardinality.SET).make();
     PropertyKey classYearKey = mgmt.makePropertyKey("classYear").dataType(Integer.class).make();
     PropertyKey workFromKey = mgmt.makePropertyKey("workFrom").dataType(Integer.class).make();
 
     // Vertex properties
-    // mgmt.addProperties(Movie, budgetKey, countriesKey, imdbIdKey, imdbRatingKey, imdbVotesKey,
-    //     languagesKey, movieIdKey, plotKey, posterKey, releasedKey, revenueKey, runtimeKey, titleKey,
-    //     tmdbIdKey, urlKey, yearKey);
+    // Message
+    mgmt.addProperties(Comment, browserUsedKey, locationIpKey, contentKey, lengthKey);
+    mgmt.addProperties(Post, browserUsedKey, locationIpKey, contentKey, lengthKey, languageKey,
+        imageFileKey); // Not modeled: language and imageFile are mutually exclusive
+    // Organization
+    mgmt.addProperties(Company, nameKey, urlKey);
+    mgmt.addProperties(University, nameKey, urlKey);
+    // Place
+    mgmt.addProperties(City, nameKey, urlKey);
+    mgmt.addProperties(Country, nameKey, urlKey);
+    mgmt.addProperties(Continent, nameKey, urlKey);
+    // Others
+    mgmt.addProperties(Forum, creationDateKey, titleKey);
+    mgmt.addProperties(Person, creationDateKey, firstNameKey, lastNameKey, genderKey, birthdayKey,
+        emailKey, speaksKey, browserUsedKey, locationIpKey);
+    mgmt.addProperties(Tag, nameKey, urlKey);
+    mgmt.addProperties(TagClass, nameKey, urlKey);
 
     // Edge labels and connections
-    // EdgeLabel ACTED_IN = mgmt.makeEdgeLabel("ACTED_IN").multiplicity(Multiplicity.SIMPLE).make();
-    // mgmt.addConnection(ACTED_IN, Actor, Movie);
+    EdgeLabel CONTAINER_OF =
+        mgmt.makeEdgeLabel("CONTAINER_OF").multiplicity(Multiplicity.ONE2MANY).make();
+    mgmt.addConnection(CONTAINER_OF, Forum, Post);
+    EdgeLabel HAS_CREATOR =
+        mgmt.makeEdgeLabel("HAS_CREATOR").multiplicity(Multiplicity.MANY2ONE).make();
+    mgmt.addConnection(HAS_CREATOR, Post, Person);
+    mgmt.addConnection(HAS_CREATOR, Comment, Person);
+    EdgeLabel HAS_INTEREST =
+        mgmt.makeEdgeLabel("HAS_INTEREST").multiplicity(Multiplicity.SIMPLE).make();
+    mgmt.addConnection(HAS_INTEREST, Person, Tag);
+    EdgeLabel HAS_MEMBER =
+        mgmt.makeEdgeLabel("HAS_MEMBER").multiplicity(Multiplicity.SIMPLE).make();
+    mgmt.addConnection(HAS_MEMBER, Forum, Person);
+    EdgeLabel HAS_MODERATOR =
+        mgmt.makeEdgeLabel("HAS_MODERATOR").multiplicity(Multiplicity.MANY2ONE).make();
+    mgmt.addConnection(HAS_MODERATOR, Forum, Person);
+    EdgeLabel HAS_TAG = mgmt.makeEdgeLabel("HAS_TAG").multiplicity(Multiplicity.SIMPLE).make();
+    mgmt.addConnection(HAS_TAG, Comment, Tag);
+    mgmt.addConnection(HAS_TAG, Post, Tag);
+    mgmt.addConnection(HAS_TAG, Forum, Tag);
+    EdgeLabel HAS_TYPE = mgmt.makeEdgeLabel("HAS_TYPE").multiplicity(Multiplicity.MANY2ONE).make();
+    mgmt.addConnection(HAS_TYPE, Tag, TagClass);
+    EdgeLabel IS_LOCATED_IN =
+        mgmt.makeEdgeLabel("IS_LOCATED_IN").multiplicity(Multiplicity.MANY2ONE).make();
+    mgmt.addConnection(IS_LOCATED_IN, Comment, Country);
+    mgmt.addConnection(IS_LOCATED_IN, Post, Country);
+    mgmt.addConnection(IS_LOCATED_IN, Company, Country);
+    mgmt.addConnection(IS_LOCATED_IN, Person, City);
+    mgmt.addConnection(IS_LOCATED_IN, University, City);
+    EdgeLabel IS_PART_OF =
+        mgmt.makeEdgeLabel("IS_PART_OF").multiplicity(Multiplicity.MANY2ONE).make();
+    mgmt.addConnection(IS_PART_OF, City, Country);
+    mgmt.addConnection(IS_PART_OF, Country, Continent);
+    EdgeLabel IS_SUBCLASS_OF =
+        mgmt.makeEdgeLabel("IS_SUBCLASS_OF").multiplicity(Multiplicity.MANY2ONE).make();
+    mgmt.addConnection(IS_SUBCLASS_OF, TagClass, TagClass);
+    EdgeLabel KNOWS = mgmt.makeEdgeLabel("KNOWS").multiplicity(Multiplicity.SIMPLE).make();
+    mgmt.addConnection(KNOWS, Person, Person);
+    EdgeLabel LIKES = mgmt.makeEdgeLabel("LIKES").multiplicity(Multiplicity.SIMPLE).make();
+    mgmt.addConnection(LIKES, Person, Comment);
+    mgmt.addConnection(LIKES, Person, Post);
+    // Not modeled: every Comment is a reply of exactly one Comment OR Post (but not both)
+    EdgeLabel REPLY_OF = mgmt.makeEdgeLabel("REPLY_OF").multiplicity(Multiplicity.MANY2ONE).make();
+    mgmt.addConnection(REPLY_OF, Comment, Comment);
+    mgmt.addConnection(REPLY_OF, Comment, Post);
+    EdgeLabel STUDY_AT = mgmt.makeEdgeLabel("STUDY_AT").multiplicity(Multiplicity.MULTI).make();
+    mgmt.addConnection(STUDY_AT, Person, University);
+    EdgeLabel WORK_AT = mgmt.makeEdgeLabel("WORK_AT").multiplicity(Multiplicity.MULTI).make();
+    mgmt.addConnection(WORK_AT, Person, Company);
 
     // Edge properties
-    // mgmt.addProperties(ACTED_IN, roleKey);
+    mgmt.addProperties(HAS_MEMBER, creationDateKey);
+    mgmt.addProperties(KNOWS, creationDateKey);
+    mgmt.addProperties(LIKES, creationDateKey);
+    mgmt.addProperties(STUDY_AT, classYearKey);
+    mgmt.addProperties(WORK_AT, workFromKey);
 
     // Set file path for all schema elements
-    // filePathByVertex.put("Movie", "movies.csv");
-    // filePathByEdge.put("ACTED_IN", "actedIn.csv");
+    filePathByVertex.put("Comment", "dynamic/comment_0_0.csv");
+    filePathByVertex.put("Post", "dynamic/post_0_0.csv");
+    filePathByVertex.put("Company", "static/organisation_0_0.csv");
+    filePathByVertex.put("University", "static/organisation_0_0.csv");
+    filePathByVertex.put("City", "static/place_0_0.csv");
+    filePathByVertex.put("Country", "static/place_0_0.csv");
+    filePathByVertex.put("Continent", "static/place_0_0.csv");
+    filePathByVertex.put("Forum", "dynamic/forum_0_0.csv");
+    filePathByVertex.put("Person", "dynamic/person_0_0.csv");
+    filePathByVertex.put("Tag", "static/tag_0_0.csv");
+    filePathByVertex.put("TagClass", "static/tagclass_0_0.csv");
+    filePathByEdge.put("CONTAINER_OF", "dynamic/forum_containerOf_post_0_0.csv");
+    filePathByEdge.put("HAS_CREATOR", "dynamic/comment_hasCreator_person_0_0.csv");
+    filePathByEdge.put("HAS_CREATOR", "dynamic/post_hasCreator_person_0_0.csv");
+    filePathByEdge.put("HAS_INTEREST", "dynamic/person_hasInterest_tag_0_0.csv");
+    filePathByEdge.put("HAS_MEMBER", "dynamic/forum_hasMember_person_0_0.csv");
+    filePathByEdge.put("HAS_MODERATOR", "dynamic/forum_hasModerator_person_0_0.csv");
+    filePathByEdge.put("HAS_TAG", "static/comment_hasTag_tag_0_0.csv");
+    filePathByEdge.put("HAS_TAG", "dynamic/forum_hasTag_tag_0_0.csv");
+    filePathByEdge.put("HAS_TAG", "dynamic/post_hasTag_tag_0_0.csv");
+    filePathByEdge.put("HAS_TYPE", "static/tag_hasType_tagclass_0_0.csv");
+    filePathByEdge.put("IS_LOCATED_IN", "static/organisation_isLocatedIn_place_0_0.csv");
+    filePathByEdge.put("IS_LOCATED_IN", "dynamic/comment_isLocatedIn_place_0_0.csv");
+    filePathByEdge.put("IS_LOCATED_IN", "dynamic/person_isLocatedIn_place_0_0.csv");
+    filePathByEdge.put("IS_LOCATED_IN", "dynamic/post_isLocatedIn_place_0_0.csv");
+    filePathByEdge.put("IS_PART_OF", "static/place_isPartOf_place_0_0.csv");
+    filePathByEdge.put("IS_SUBCLASS_OF", "static/tagclass_isSubclassOf_tagclass_0_0.csv");
+    filePathByEdge.put("KNOWS", "dynamic/person_knows_person_0_0.csv");
+    filePathByEdge.put("LIKES", "dynamic/person_likes_comment_0_0.csv");
+    filePathByEdge.put("LIKES", "dynamic/person_likes_post_0_0.csv");
+    filePathByEdge.put("REPLY_OF", "dynamic/comment_replyOf_comment_0_0.csv.csv");
+    filePathByEdge.put("REPLY_OF", "dynamic/comment_replyOf_post_0_0.csv");
+    filePathByEdge.put("STUDY_AT", "dynamic/person_studyAt_organisation_0_0.csv");
+    filePathByEdge.put("WORK_AT", "dynamic/person_workAt_organisation_0_0.csv");
 
     mgmt.commit();
   }
@@ -98,6 +185,9 @@ public class SNBModel extends DataModel {
   }
 
   public void loadData(File dataDir) throws IOException, ParseException {
-    
+    // TODO: deal with different vertex labels coming from same file (place/organisation)
+    // TODO: deal with same edge label ccoming from multiple files
+    // TODO: deal with set properties being separate files (email/speaks)
+    throw new NotImplementedException();
   }
 }
